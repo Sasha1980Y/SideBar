d69d9d00b92f607a428b5580d96fffb6e630fe09
AlexanderYakovenko <AlexanderYakovenko@Hellens-Mac-mini.local>
2017-07-12 14:50:01 +0300

    worked sidebar and main view with shadow

--- SideBar/ViewController.swift
+++ SideBar/ViewController.swift
@@ -9,9 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var trailingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
+    var menuShowing = false
+    
+    @IBOutlet weak var mainView: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainView.layer.shadowOpacity = 1
+        mainView.layer.shadowRadius = 10
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +33,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showMenu(_ sender: Any) {
+        
+        if menuShowing {
+            leadingConstraint.constant = 0
+            trailingConstraint.constant = 0
+        } else {
+            leadingConstraint.constant = 100
+            trailingConstraint.constant = -100
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        
+        
+        
+        menuShowing = !menuShowing
+        
+        
+    }
 
 }
 
